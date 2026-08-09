82a03a8e06d7ede00577e054d91ca62b5e898f1a
Yusuf Celal Çelik <info@yusufcelalcelik.com>
2026-08-09 12:00:49 +0300

    feat: otp endpoininte mail eklendi

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import bcrypt from "bcrypt";
 import sql from "./config/db.js";
 import redisClient from "./config/redis.js";
-
+import transporter from "./config/mailer.js";
 const app = express();
 const router = express.Router();
 
@@ -13,74 +13,86 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ status: "ok" });
+  res.status(200).json({ status: "ok" });ƒ
 });
 
 //Geçici olarak kullanıcıları listele
 router.get("/data", async (req, res) => {
-
-    //TEST için veri tabanına kolay erişim
-    const response = await sql`SELECT * FROM users`
-    res.status(200).send(response)
-})
+  //TEST için veri tabanına kolay erişim
+  const response = await sql`SELECT * FROM users`;
+  res.status(200).send(response);
+});
 
 //Yeni doğrulama kodu gönder
 router.put("/otp", async (req, res) => {
-    const { email } = req.body;
+  const { email } = req.body;
 
-    //Doğrulama kodunu oluştur ve set et
-    const otp = crypto.randomInt(100000, 1000000)
-    await redisClient.set(`otp:${email}`, otp, { EX: 300 })
-
-    //Yolla
-    res.status(200).json({ success: true })
-})
+  //Doğrulama kodunu oluştur ve set et
+  const otp = crypto.randomInt(100000, 1000000);
+  await redisClient.set(`otp:${email}`, otp, { EX: 300 });
+  await transporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to: email,
+    subject: "Doğrulama Kodu",
+    text: `Doğrulama kodunuz: ${otp}`,
+  });
+  //Yolla
+  res.status(200).json({ success: true });
+});
 
 //Giriş Yap
 router.post("/login", async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    // Kullanıcı bulunur
-    const [user] = await sql`SELECT password FROM users WHERE username = ${username}`
-    if (!user) return res.status(401).json({ error: "Username or Password incorrect" });
+  // Kullanıcı bulunur
+  const [user] =
+    await sql`SELECT password FROM users WHERE username = ${username}`;
+  if (!user)
+    return res.status(401).json({ error: "Username or Password incorrect" });
 
-    // Bcrypt ile password eşleşmesi kontrol edilir. 
-    const isValid = await bcrypt.compare(password, user.password)
-    if (!isValid) return res.status(401).json({ error: "Username or Password incorrect" });
+  // Bcrypt ile password eşleşmesi kontrol edilir.
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid)
+    return res.status(401).json({ error: "Username or Password incorrect" });
 
-    //Bilgiler doğru ise kullanııcı bilgilerini çek ve yolla
-    const [response] = await sql`SELECT uuid, name, username, post, follow, followers, bio FROM users WHERE username = ${username}`
-    res.send(response)
-})
+  //Bilgiler doğru ise kullanııcı bilgilerini çek ve yolla
+  const [response] =
+    await sql`SELECT uuid, name, username, post, follow, followers, bio FROM users WHERE username = ${username}`;
+  res.send(response);
+});
 
 //Kayıt ol
 router.post("/register", async (req, res) => {
-    const { username, name, email, password } = req.body;
+  const { username, name, email, password } = req.body;
 
-    // Kullanıcı adı ya da email zaten varsa kontrolü
-    const [{ exists }] = await sql`SELECT EXISTS(SELECT 1 FROM users WHERE username = ${username} OR email = ${email}) AS exists`
-    if (exists) return res.status(409).json({ error: "Bu kullanıcı adı veya email zaten kullanılıyor" })
+  // Kullanıcı adı ya da email zaten varsa kontrolü
+  const [{ exists }] =
+    await sql`SELECT EXISTS(SELECT 1 FROM users WHERE username = ${username} OR email = ${email}) AS exists`;
+  if (exists)
+    return res
+      .status(409)
+      .json({ error: "Bu kullanıcı adı veya email zaten kullanılıyor" });
 
-    // Eşleşme yoksa şifreyi hash leyio kaydedelim
-    const hash = await bcrypt.hash(password, 10)
-    const [user] = await sql`
+  // Eşleşme yoksa şifreyi hash leyio kaydedelim
+  const hash = await bcrypt.hash(password, 10);
+  const [user] = await sql`
         INSERT INTO users (username, name, email, password)
         VALUES (${username}, ${name}, ${email}, ${hash})
         RETURNING uuid, name, username, post, follow, followers, bio
-    `
-    //Yolla
-    res.status(201).json(user)
-})
+    `;
+  //Yolla
+  res.status(201).json(user);
+});
 
 app.use("/api", router);
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: "Sunucu hatası" });
+  console.error(err);
+  res.status(500).json({ error: "Sunucu hatası" });
 });
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
