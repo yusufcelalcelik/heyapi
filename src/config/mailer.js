84a3de21dbcbc0b115497b0bedba640e7d404484
Yusuf Celal Çelik <info@yusufcelalcelik.com>
2026-08-09 12:00:35 +0300

    feat: mailer eklendi

---
+++ src/config/mailer.js
@@ -0,0 +1,13 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export default transporter;
